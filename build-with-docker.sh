3425f7b01950c75a913b5baebcff9175b730a38b
William Tange <will@tan.ge>
2024-04-06 18:49:11 +0200

    Remove UID/GID override in build-with-docker.sh

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -16,7 +16,6 @@ docker run \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
-    -u $(id -u):$(id -g) \
     $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
     emception_build:latest \
     bash -c "cd $(pwd) && ./build.sh"
