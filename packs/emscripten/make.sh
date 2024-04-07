c220ce9410ec0401f868f87dc7d188b45fc5912c
William Tange <will@tan.ge>
2024-04-07 09:33:45 +0200

    Fix ES module error

--- packs/emscripten/make.sh
+++ packs/emscripten/make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -d emscripten ]; then
-    # nothing to do here
-    exit
+	# nothing to do here
+	exit
 fi
 
 SRC=$(dirname $0)
@@ -20,33 +20,33 @@ pushd emscripten/
 cp $SRC/config ./.emscripten
 
 # We won't support closure-compiler, remove it from the dependencies
-cat package.json \
-    | jq '. | del(.dependencies["google-closure-compiler"])' \
-    | jq '. | del(.dependencies["html-minifier-terser"])' \
-    > _package.json
+cat package.json |
+	jq '. | del(.dependencies["google-closure-compiler"])' |
+	jq '. | del(.dependencies["html-minifier-terser"])' \
+		>_package.json
 mv _package.json package.json
 
 # Patch emscripten to:
 # * avoid invalidating the cache
 # * fix a bug with proxy_to_worker
-patch -p2 < $SRC/emscripten.patch
+patch -p2 <$SRC/emscripten.patch
 
 # Install dependencies (but nor development dependencies)
 npm i --only=prod
 
 # Remove a bunch of things we won't use
 rm -Rf \
-    ./.circleci \
-    ./.github \
-    ./cmake \
-    ./site \
-    ./test \
-    ./third_party/closure-compiler \
-    ./third_party/jni \
-    ./third_party/ply \
-    ./third_party/websockify \
-    ./tools/websocket_to_posix_proxy \
-    ./*.bat
+	./.circleci \
+	./.github \
+	./cmake \
+	./site \
+	./test \
+	./third_party/closure-compiler \
+	./third_party/jni \
+	./third_party/ply \
+	./third_party/websockify \
+	./tools/websocket_to_posix_proxy \
+	./*.bat
 
 CONTAINER_ID=$(docker create emscripten/emsdk:3.1.24)
 docker cp $CONTAINER_ID:/emsdk/upstream/emscripten/cache ./cache
@@ -56,36 +56,36 @@ mkdir -p ./lazy-cache
 LAZY_CACHE_CONTENTS=./lazy-cache/contents.mjs
 LAZY_CACHE_IMPORTS=./lazy-cache/imports.mjs
 LAZY_CACHE_MODULE=./lazy-cache/index.mjs
-echo "export default [" >> $LAZY_CACHE_CONTENTS
+echo "export default [" >>$LAZY_CACHE_CONTENTS
 
 shopt -s globstar
 for FILE in ./cache/**/*.a; do
-    MD5="$(md5sum "$FILE")"
-    MD5="${MD5%% *}"
-    SIZE="$(stat --printf="%s" "$FILE")"
-    if ! [ -e "./lazy-cache/$MD5.a" ]; then
-        echo "import p$MD5 from \"./$MD5.a\";" >> $LAZY_CACHE_IMPORTS
-    fi
-    mv -f "$FILE" "./lazy-cache/$MD5.a"
+	MD5="$(md5sum "$FILE")"
+	MD5="${MD5%% *}"
+	SIZE="$(stat --printf="%s" "$FILE")"
+	if ! [ -e "./lazy-cache/$MD5.a" ]; then
+		echo "import p$MD5 from \"./$MD5.a\";" >>$LAZY_CACHE_IMPORTS
+	fi
+	mv -f "$FILE" "./lazy-cache/$MD5.a"
 
-    {
-        echo -n '    ['
-        echo -n "$FILE" | jq -sR | tr -d '\n'
-        echo -n ','
-        echo -n "$SIZE"
-        echo -n ','
-        echo -n "$MD5" | jq -sR | tr -d '\n'
-        echo -n ','
-        echo -n "p$MD5"
-        echo '],'
-    } >> $LAZY_CACHE_CONTENTS
+	{
+		echo -n '    ['
+		echo -n "$FILE" | jq -sR | tr -d '\n'
+		echo -n ','
+		echo -n "$SIZE"
+		echo -n ','
+		echo -n "$MD5" | jq -sR | tr -d '\n'
+		echo -n ','
+		echo -n "p$MD5"
+		echo '],'
+	} >>$LAZY_CACHE_CONTENTS
 done
 
-echo ""  >> $LAZY_CACHE_IMPORTS
+echo "" >>$LAZY_CACHE_IMPORTS
 
-echo "];"  >> $LAZY_CACHE_CONTENTS
+echo "];" >>$LAZY_CACHE_CONTENTS
 
-cat $LAZY_CACHE_IMPORTS $LAZY_CACHE_CONTENTS > $LAZY_CACHE_MODULE
+cat $LAZY_CACHE_IMPORTS $LAZY_CACHE_CONTENTS >$LAZY_CACHE_MODULE
 rm $LAZY_CACHE_IMPORTS $LAZY_CACHE_CONTENTS
 
 popd
